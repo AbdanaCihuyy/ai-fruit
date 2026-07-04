7e61877c4ea276cbb80f709e54e2f9cf9cb4bb46
AbdanaCihuyy <abdanadwiguntara2708@gmail.com>
2026-07-05 04:05:50 +0700

    Add files via upload

---
+++ fruit-ai-main/config.js
@@ -0,0 +1,12 @@
+/*
+=========================================
+ Gemini Configuration
+=========================================
+*/
+
+const CONFIG = {
+    MODEL: "gemini-2.5-flash",
+    ENDPOINT: "https://generativelanguage.googleapis.com/v1beta/models"
+};
+
+let API_URL = "";
